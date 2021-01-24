8f19a4d4a1ebae1a8f1dd2d0e3106a819d7b4194
Tiana <Tiana.marie013@gmail.com>
2021-01-24 12:07:28 -0700

    map

--- src/App.js
+++ src/App.js
@@ -1,62 +1,77 @@
-import React, { useState, useEffect, useRef, createContext, useMemo} from 'react';
-import Toggle from './Toggle.js';
-import Counter from './Counter';
-import { useTitleInput } from './hooks/useTitleInput';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  createContext,
+  useMemo,
+} from "react"
+import Toggle from "./Toggle.js"
+import Counter from "./Counter"
+import { useTitleInput } from "./hooks/useTitleInput"
 
-export const userContext = createContext();
+export const userContext = createContext()
 //useState can only be used on a function based component
 // if using class base components, use State
 const App = () => {
-
   // const [value, setValue] = useState(initialState);
-  const [name, setName] = useTitleInput('');
-  const ref = useRef();
+  const [name, setName] = useTitleInput("")
+  const ref = useRef()
+  const [dishes, setDishes] = useState([])
 
-  const reverseWord = word => {
-    console.log('function called')
-    return word.split("").reverse().join("");
+  const reverseWord = (word) => {
+    console.log("function called")
+    return word.split("").reverse().join("")
   }
 
-    const title = 'Level up Dishes';
- 
-// caches. The [] checks to see if the item has changed.
-// if it has not, it will not update.
-  const TitleReversed = useMemo(() => reverseWord(name), [name]) 
-  
+  const title = "Level up Dishes"
+
+  // caches. The [] checks to see if the item has changed.
+  // if it has not, it will not update.
+  const TitleReversed = useMemo(() => reverseWord(name), [name])
 
   // useEffect(()=> {
   //   document.title = name;  //<-document.title targets the tab
   // })
 
-
-
   return (
     <userContext.Provider
-        value={{
-          user:false
-        }}>
-    <div className="main-wrapper" ref = {ref}>
-      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
-    
-      <Toggle/>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-      }}>
-     
-      <input type="text"
-             onChange={(e) => setName(e.target.value)} 
-             value={name} />
+      value={{
+        user: false,
+      }}
+    >
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
+          Level Up Dishes
+        </h1>
+        <Toggle />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
           <button> Submit</button>
-      </form>
-      <Counter/>
-
-    </div>
+        </form>
+        <Counter />
+        {dishes.map((dish) => (
+          <article className=" dish-card dish-card--withImage">
+            <h3>{dish.name}</h3>
+            <p>{dish.desc}</p>
+            <div className="ingredients">
+              {dish.ingredients.map(ingredient => (
+                <span>{ingredient}</span>
+              ))}
+            </div>
+          </article>
+        ))}
+        ;
+      </div>
     </userContext.Provider>
-  );
-};
+  )
+}
 
-
-
-
-
-export default App;
+export default App
