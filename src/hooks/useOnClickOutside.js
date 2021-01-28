b8f90a2c8ac470290c37867772a15fdc513dce1c
Tiana <Tiana.marie013@gmail.com>
2021-01-27 18:43:48 -0700

    clean up

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -9,6 +9,11 @@ function useOnClickOutside(ref){
 
         document.addEventListener('mousedown', listener);
         document.addEventListener('touchstart', listener);
+
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        }
         
     }, [])
 }
