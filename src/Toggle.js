98c5a6264c2157ba144f466cd5ff053628fcebaf
Tiana <Tiana.marie013@gmail.com>
2021-01-27 18:58:55 -0700

    turnery

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,35 +1,36 @@
-import React, { useState, useContext } from "react";
-import DishForm from "./DishForm";
-import { userContext } from './App';
+import React, { useState, useContext } from "react"
+import DishForm from "./DishForm"
+import { userContext } from "./App"
 
 const Toggle = () => {
+  const [isToggled, setToggle] = useState(false)
+  // const userInfo = useContext(userContext);
+  // if(!userInfo.user) return null;
 
-    const [isToggled, setToggle] = useState(false);
-    // const userInfo = useContext(userContext);
-    // if(!userInfo.user) return null;
+  return (
+    <div>
+      {isToggled ? (
+        <DishForm setToggle={setToggle} />
+      ) : (
+        <button onClick={() => setToggle(!isToggled)}>Open</button>
+      )}
+    </div>
+  )
+}
 
-    return (
-        <div>
-           <button onClick={() => setToggle(!isToggled)}>Toggle</button>
-           {isToggled && <DishForm setToggle={setToggle}/>}
-         </div>
-    );
-};
-
-export default Toggle;
-
+export default Toggle
 
 // export default class Refactor extends Component {
 //     state = {
 //       isToggled: false
 //     };
-  
+
 //     toggle = () => {
 //       this.setState(state => {
 //         return { isToggled: !state.isToggled };
 //       });
 //     };
-  
+
 //     render() {
 //       return (
 //         <div>
